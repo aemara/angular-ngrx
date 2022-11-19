beab7b9f517368a62add04462dbab9f003950bfd
Amr Emara <a.emara47@gmail.com>
2022-11-19 04:14:11 +0200

    add httpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { BooksComponent } from '../books/books.component';
-import { BooksService } from '../services/books.service';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { BooksComponent } from "../books/books.component";
+import { BooksService } from "../services/books.service";
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   declarations: [AppComponent, HelloComponent, BooksComponent],
   bootstrap: [AppComponent],
   providers: [BooksService],
